7dac39bd1024ecfd7a3f433114dcb0e40f0e4522
Paulo Rodrigues <paulo.rodrigues@aurea.com>
2020-07-24 21:26:16 -0300

    Add eslint and fixes findings

--- src/Tabela.js
+++ src/Tabela.js
@@ -1,54 +1,52 @@
-import React, {Component} from "react";
+import React from 'react';
 
-const TableHead = () => {
-    return (
-            <thead>
-            <tr>
-                <th>Autor</th>
-                <th>Titulo</th>
-                <th>Preco</th>
-                <th>Remover</th>
+const TableHead = () => (
+        <thead>
+        <tr>
+            <th>Autor</th>
+            <th>Titulo</th>
+            <th>Preco</th>
+            <th>Remover</th>
+        </tr>
+        </thead>
+);
+
+const TableBody = ({autores, removeAutor}) => {
+    const linhas = autores.map((linha, index) => (
+            <tr key={linha.nome - linha.titulo}>
+                <td>{linha.nome}</td>
+                <td>{linha.titulo}</td>
+                <td>{linha.preco}</td>
+                <td>
+                    <button
+                            type="submit"
+                            onClick={() => removeAutor(index)}
+                    >
+                        Remover
+                    </button>
+                </td>
             </tr>
-            </thead>
-    );
-}
-
-const TableBody = (props) => {
-    const linhas = props.autores.map((linha, index) => {
-        return (
-                <tr key={index}>
-                    <td>{linha.nome}</td>
-                    <td>{linha.titulo}</td>
-                    <td>{linha.preco}</td>
-                    <td>
-                        <button onClick={() => props.removeAutor(index)}>
-                            Remover
-                        </button>
-                    </td>
-                </tr>
-        );
-    });
+    ));
 
     return (
             <tbody>
             {linhas}
             </tbody>
     );
-}
+};
 
-class Tabela extends Component {
-    render() {
+function Tabela(props) {
+    const {autores, removeAutor} = props;
 
-        const {autores, removeAutor} = this.props;
-
-        return (
-                <table>
-                    <TableHead/>
-                    <TableBody autores={autores}
-                               removeAutor={removeAutor}/>
-                </table>
-        );
-    }
+    return (
+            <table>
+                <TableHead/>
+                <TableBody
+                        autores={autores}
+                        removeAutor={removeAutor}
+                />
+            </table>
+    );
 }
 
 export default Tabela;
