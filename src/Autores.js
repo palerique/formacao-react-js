98d1c581f463561f28c58ec0b0afa2abaf631b60
Paulo Rodrigues <paulo.rodrigues@aurea.com>
2020-07-25 21:24:30 -0300

    Improve paginas de autores e livros

--- src/Autores.js
+++ src/Autores.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './Header';
+import DataTable from './DataTable';
 
-const Autores = () => {
-  return (
-    <>
-      <Header />
-      <h1>Autores</h1>
-    </>
-  );
-};
+class Autores extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      autores: [
+        {
+          nome: 'Paulo',
+          livro: 'React',
+          preco: '1000',
+        },
+        {
+          nome: 'Daniel',
+          livro: 'Java',
+          preco: '99',
+        },
+        {
+          nome: 'Marcos',
+          livro: 'Design',
+          preco: '150',
+        },
+        {
+          nome: 'Bruno',
+          livro: 'DevOps',
+          preco: '100',
+        },
+        {
+          nome: 'Nico',
+          livro: 'Java',
+          preco: '9999',
+        },
+      ],
+      titulo: 'Autores',
+    };
+  }
+
+  render() {
+    const {titulo, autores} = this.state;
+    return (
+      <>
+        <Header />
+        <div className="container">
+          <h1>Página de Autores</h1>
+          <DataTable dados={autores} titulo={titulo} colunas={['nome']} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default Autores;
--- src/Livros.js
+++ src/Livros.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './Header';
+import DataTable from './DataTable';
 
-const Livros = () => {
-  return (
-    <>
-      <Header />
-      <h1>Livros</h1>
-    </>
-  );
-};
+class Livros extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      autores: [
+        {
+          nome: 'Paulo',
+          livro: 'React',
+          preco: '1000',
+        },
+        {
+          nome: 'Daniel',
+          livro: 'Java',
+          preco: '99',
+        },
+        {
+          nome: 'Marcos',
+          livro: 'Design',
+          preco: '150',
+        },
+        {
+          nome: 'Bruno',
+          livro: 'DevOps',
+          preco: '100',
+        },
+        {
+          nome: 'Nico',
+          livro: 'Java',
+          preco: '9999',
+        },
+      ],
+      titulo: 'Livros',
+    };
+  }
+
+  render() {
+    const {titulo, autores} = this.state;
+    return (
+      <>
+        <Header />
+        <div className="container">
+          <h1>Página de Livros</h1>
+          <DataTable dados={autores} titulo={titulo} colunas={['livro']} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default Livros;
